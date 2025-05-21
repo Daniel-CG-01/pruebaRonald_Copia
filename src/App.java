bc9c5c0937a651cc37c9a7790ddafc6ace7b1b7e
Daniel <danielzanon2005@gmail.com>
2025-05-21 13:01:27 +0200

    Proyecto cambiado

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Daniel!");
+        System.out.println("Ya no soy Ronald, soy Daniel");
     }
 }
